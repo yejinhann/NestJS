de719a61cba3868bb850705d224ef14292c907e7
yejinhann <yejin@lattice.im>
2025-10-04 14:30:07 +0900

    feat: 유저 생성 API 추가

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
 }
